91a852f0e43292a587f907e0cb6b0101a827796e
onubrooks <abahonuh@gmail.com>
2023-11-18 02:30:25 +0100

    set up airflow along with directory structure

--- airflow/transform/job.py
+++
@@ -1,8 +0,0 @@
-from transform_tasks import transform_players_to_parquet, transform_matches_to_parquet, transform_rankings_to_parquet
-
-def transform_tennis_data(FILE_SAVE_LOCATION, MATCH_YEARS, RANK_YEARS):
-    print('Transforming tennis data')
-    transform_players_to_parquet(FILE_SAVE_LOCATION)
-    transform_matches_to_parquet(FILE_SAVE_LOCATION, MATCH_YEARS)
-    transform_rankings_to_parquet(FILE_SAVE_LOCATION, RANK_YEARS)
-    print('Tennis data transformed and saved to parquet')
